de692e2bcfaad6db68daed0cf1363aab4554cae1
bhupendra <bhupendra@byottech.com>
2022-04-05 10:44:07 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider} from "@ethersproject/providers" 
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider} from "@chakra-ui/react"
+import { ethers } from "ethers"
+
+function getLibrary(provider) {
+  const library = new ethers.provider.Web3Provider(provider);
+  library.pollingIntervsl = 8000;
+  return library;
+};
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+          <App/>
+      </Web3ReactProvider>
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
